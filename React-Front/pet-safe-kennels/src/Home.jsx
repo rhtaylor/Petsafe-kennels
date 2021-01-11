080787d2c3d39280f74d26e21ff2e059ec7a63a0
Randy <randyisbusy@gmail.com>
2021-01-10 19:22:01 -0700

    adds new homepage with maped image for different links

--- React-Front/pet-safe-kennels/src/Home.jsx
+++ React-Front/pet-safe-kennels/src/Home.jsx
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 import kennel from './kennel.jpg'  
 import Kennel from './Kennel.jsx' 
 import home from './favicon.ico';
-import './home.css'  
+import './home.css'   
+import './home.scss'
 import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom'; 
 const info = "We Can Help!   No Matter Where You Live. A rescued pet should not be threatened by aggressive animals, wild or domestic. Nationwide Pet Lovers are increasingly at risk of losing our furry family members to Coyotes, Bobcats, Wolves, Hawks, Mountain Lions, Aggressive Dogs, & Snakes that may jeapordize your dogs, cats, and birds"
 const post_info = "We started building kennels that are safe for pets in 2006 after my daughter's dachshund was killed by a coyote. Ten years and over 2,500 predator proof dog & cat enclosures later, we have been called the Michelangelo of dog kennels. Call Us for a Free Estimate. It's our years of experience that makes us better than other dog kennels companies because with each kennel we research and develop new techniques, methods, and proprietary designs that other comapnies can not duplicate. We build and sell the Absolute STRONGEST Kennels You Can Find Anywhere!   Guaranteed!"
@@ -22,20 +23,24 @@ export default class Home extends Component{
     
     }
     render(){
-        return(
-            <header> 
+        return( 
+            <div id="container">
+            <header id="home"> 
                 <Route path='/Kennel' component={Kennel} />
                 <div id="home" > 
-                <img alt="home" src={home} usemap="#imgmap" onClick={e=>this.mouseFinder(e)}/> 
+                <img alt="home"  src={home} usemap="#imgmap" onClick={e=>this.mouseFinder(e)}/> 
                 <map name="imgmap">
                 <area onClick={e=>this.dog(e)} shape="dog" coords="149,180,229,190" href="http://localhost:3001/Kennel" alt="dog">
                 </area>
                 </map>
-                <p>{info}</p> 
-                <p>{post_info}</p>
                 </div> 
                 
-            </header>
+            </header> 
+            <footer>
+                 <p>{info}</p> 
+                <p>{post_info}</p>
+            </footer>  
+            </div>
         )
     }
 }
