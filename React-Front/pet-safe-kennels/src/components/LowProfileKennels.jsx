1a2a8b62195b149b681182cd7f776ea79017578d
Randy <randyisbusy@gmail.com>
2021-02-07 10:42:39 -0700

    child component for small dog kennels in kennel.jsx

---
+++ React-Front/pet-safe-kennels/src/components/LowProfileKennels.jsx
@@ -0,0 +1,13 @@
+import React, { Component } from 'react' 
+import '../css/small-dog-kennel.scss'
+export default class LowProfile extends Component{
+    render(){
+        return( <div>
+                <div id="small-dog-kennel" >
+                <p>SMALL DOG OUTDOOR KENNELS</p>
+              </div>  
+              </div>
+
+        )
+    }
+}
