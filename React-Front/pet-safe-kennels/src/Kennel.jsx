4fd873c33e973bb024cf08d3947d1f3cbb9b6e2f
Randy <randyisbusy@gmail.com>
2021-02-17 18:22:18 -0700

    adds new BigKennel component to subpage: kennels

--- React-Front/pet-safe-kennels/src/Kennel.jsx
+++ React-Front/pet-safe-kennels/src/Kennel.jsx
@@ -1,6 +1,7 @@
 import React, { Component } from 'react' 
 import LowProfile from './components/LowProfileKennels.jsx'
-import LowProfileBack from './components/LowProfileKennelsBack.jsx'
+import LowProfileBack from './components/LowProfileKennelsBack.jsx' 
+import BigKennel from './components/BigKennel.jsx'
 const info = "We Can Help!   No Matter Where You Live. A rescued pet should not be threatened by aggressive animals, wild or domestic. Nationwide Pet Lovers are increasingly at risk of losing our furry family members to Coyotes, Bobcats, Wolves, Hawks, Mountain Lions, Aggressive Dogs, & Snakes that may jeapordize your dogs, cats, and birds"
 const post_info = "We are here to protect your pets, make them comfortable, and keep them happy. We have been called the Michelangelo of dog kennels. Call Us for a Free Estimate. It's our years of experience that makes us better than other dog kennels companies because with each kennel we research and develop new techniques, methods, and proprietary designs that other comapnies can not duplicate. We build and sell the Absolute STRONGEST Kennels You Can Find Anywhere!   Guaranteed!"
 
@@ -34,8 +35,11 @@ export default class Kennel extends Component{
             <h1>Custom Built Kennels Designed To Keep Pets Safe!</h1> 
             <p>{info}</p>
             <p>{post_info}</p>  
+            <div id="sub-kennel">
             <LowProfile show={this.state.front} clickMe={e=>this.flipKennel(e)} /> 
             <LowProfileBack push={this.props.history} show={this.state.back} clickMe={e => this.flipKennel(e)}/>
+            <BigKennel /> 
+            </div>
         </div>
         )
     }
---
+++ React-Front/pet-safe-kennels/src/components/BigKennel.jsx
@@ -0,0 +1,11 @@
+import React, { Component } from 'react'
+import '../css/big-kennel.scss'
+export default class BigKennel extends Component{
+    
+    render(){
+        return(<div id="big-dog-kennel">
+            <p>LARGE DOG OUTDOOR KENNELS</p>
+
+        </div>)
+    }
+}
