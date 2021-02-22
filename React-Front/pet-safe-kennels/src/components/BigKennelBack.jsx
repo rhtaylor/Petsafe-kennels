dc15d157e67080ad5514233a430aa577339eb5b5
Randy <randyisbusy@gmail.com>
2021-02-22 10:21:37 -0700

    updates content and fnxalit of BigKennel component

--- React-Front/pet-safe-kennels/src/components/BigKennelBack.jsx
+++ React-Front/pet-safe-kennels/src/components/BigKennelBack.jsx
@@ -1,11 +1,24 @@
 import React, { Component } from 'react'
+import KennelBtn from './KennelBtn.jsx'
 import '../css/big-kennel.scss'
 export default class BigKennelBack extends Component{
-    render(){
+    
+    myRouting(e) {
+        debugger
+        e.preventDefault()
+        this.props.push.push('BigDogKennels')
+
+    }
+    render(){           
         return (<div id="big-kennel-back" style={{ display: this.props.show }}
             key={this.props.show}
             onClick={e => this.props.clickMe(e)}>
-            <p>YO</p>
+            <p>Our Big Dog model will keep your Big Dog Happy.</p> 
+            <ul><li>Extra Space for Play</li>
+                <li>Full Door from for Owners+Dogs.</li>
+                <li>Can connect to your house via a doggy door.</li>
+                <li><KennelBtn myroute={this.myRouting} /></li>
+            </ul> 
         </div>)
     }
 }
