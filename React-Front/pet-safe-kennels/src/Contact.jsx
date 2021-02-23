263ca3fae36ddeace0a7055f494eb474dec1ad0d
Randy <randyisbusy@gmail.com>
2021-02-23 13:22:37 -0700

    update to contact component

--- React-Front/pet-safe-kennels/src/Contact.jsx
+++ React-Front/pet-safe-kennels/src/Contact.jsx
@@ -1,10 +1,45 @@
 import React, { Component } from 'react' 
+import ContactUs from './EmailForm.jsx'
+import Button from './Button.jsx'
+import './css/button.scss'
 
 export default class Contact extends Component{
+    constructor(props){
+        super(props)
+        this.state={
+            display: null
+        }
+        this.updateForm = this.updateForm.bind(this)
+    
+    }
+    updateForm(e) {
+        e.preventDefault()
+        debugger
+        this.state.display == null
+            ?
+            this.setState(
+                {
+                    display: "flex",
+                    direction: 'column'
+                })
+            :
+            this.setState({
+                display: null,
+                direction: null
+            })
+
+    } 
     render(){
-        return(<div>
+        return(<div id="dog-parent">
+            <Button handleClick={(e) => this.updateForm(e)} />
+            <ContactUs display={{ display: this.state.display }} stateToProp={this.state.direction} history={this.props.history}
+                updateForm={this.updateForm}
+            /> 
+            <p>Phone calls are the preferred way to communicate with us.</p> 
             <p>Contact info</p>
-            <p>call: 520-730-7020</p>
+            <p id="call">please call: 520-730-7020</p>  
+            <p id="highlight">Important: Pet-Safe-Kennels Custom builds kennels by hand with wielders. These craftsmen
+            will build the perfect kennel for you, your home, and your pet. The minimum price per Kennel is $2000. However that is also the price of a purebred pitbull. These kennels will outlast your car.</p>
         </div>)
     }
 }
