a5a1b1b5cda8d124ed00212ae240aa0b43b81fc0
Randy <randyisbusy@gmail.com>
2021-03-06 16:59:43 -0700

    adds constructor and state to controle of display

--- React-Front/pet-safe-kennels/src/components/All-Dog-Kennels.jsx
+++ React-Front/pet-safe-kennels/src/components/All-Dog-Kennels.jsx
@@ -9,11 +9,17 @@ import WrapAroundKennel from '../pictures/WrapAroundKennel.jpg'
 const DOGARRAY = [{ title: 'Klassic Kennel', info: 'Classic Small Dog Kennel', img: small, size: '6x3' }, { title: 'Wrap Around', info: 'Custom kennel to wrap to the contours of the house. Attached to the house with doggy-door.', img: WrapAroundKennel, size: '20x3' },{ title: 'Cool and low-down', info: 'Low profile and out of the way of your home, custom built to fit in house window nook', size: '7x2.5', img: low }, { title: 'Perfection', info: 'small dog kennel attached to house with doggy-door and dog-door', size: '3x5', img: lowProfileDogkennel }]
 export default class AllDogKennelsPage extends Component{
    constructor(props){
-    super(props) 
-    this.make_kennels = this.make_kennels.bind(this)
-   } 
+    super(props)  
+       this.state = {
+           display: 'show',
+           front: 'show',
+           back: 'none',
+           }
+    this.make_kennels = this.make_kennels.bind(this) 
+    
+    } 
    make_kennels(){
-       return DOGARRAY.map((k,i) => <span><DogKennel kennel={k}/><DogKennelBack kennel={k}/></span>)
+       return DOGARRAY.map((k,i) => <span><DogKennel kennel={k}/><DogKennelBack display={this.state.back} kennel={k}/></span>)
    }
     render(){
         return(<div id="kennel_slide">
--- React-Front/pet-safe-kennels/src/components/DogKennelBack.jsx
+++ React-Front/pet-safe-kennels/src/components/DogKennelBack.jsx
@@ -1,8 +1,11 @@
 import React, { Component } from 'react'
 export default class DogKennelBack extends Component{
+    constructor(props){
+        super(props)
+    }
     render(){
         return(
-            <div id="kennel_back">
+            <div id="kennel_back" style={{display: this.props.display}}>
                 <div className="parentDog" key={this.props.kennel.title[0]}>
                     <div className="myKennels" key={this.props.kennel[1]}>
                         <h3>{this.props.kennel.title}</h3>
