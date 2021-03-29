4f59b1336fc6b6d6314c24e3cd059d722dd98225
Randy <randyisbusy@gmail.com>
2021-03-29 14:24:04 -0700

    updates to Kennel for @media query

--- React-Front/pet-safe-kennels/src/Kennel.jsx
+++ React-Front/pet-safe-kennels/src/Kennel.jsx
@@ -3,7 +3,8 @@ import LowProfile from './components/LowProfileKennels.jsx'
 import LowProfileBack from './components/LowProfileKennelsBack.jsx' 
 import BigKennel from './components/BigKennel.jsx'
 import BigKennelBack from './components/BigKennelBack.jsx' 
-import './css/kennel.scss'
+import './css/kennel.scss' 
+import './css/App.scss'
 const info = "We Can Help!   No Matter Where You Live. A rescued pet should not be threatened by aggressive animals, wild or domestic. Nationwide Pet Lovers are increasingly at risk of losing our furry family members to Coyotes, Bobcats, Wolves, Hawks, Mountain Lions, Aggressive Dogs, & Snakes that may jeapordize your dogs, cats, and birds"
 const post_info = "We are here to protect your pets, make them comfortable, and keep them happy. We have been called the Michelangelo of dog kennels. Call Us for a Free Estimate. It's our years of experience that makes us better than other dog kennels companies because with each kennel we research and develop new techniques, methods, and proprietary designs that other comapnies can not duplicate. We build and sell the Absolute STRONGEST Kennels You Can Find Anywhere!   Guaranteed!"
 
