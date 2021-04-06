2880904bdf6f4cdea1a553172bf8e22474b2ab88
Randy <randyisbusy@gmail.com>
2021-04-06 14:36:20 -0700

    update to DogRun

--- React-Front/pet-safe-kennels/src/components/DogRunFront.jsx
+++ React-Front/pet-safe-kennels/src/components/DogRunFront.jsx
@@ -2,17 +2,30 @@ import React from 'react'
 import { Component } from 'react';
 import '../css/dogrun.scss'
 export default class DogRunFront extends Component{
-    
-    render(){
-        return(<div id="open-dog-run"
-                onClick={e => this.props.clickMe(e)}
+    constructor(props) {
+        super(props) 
+        this.openUP = this.openUP.bind(this)
+        this.state = {
+            'min-width': '400px',
+            position: 'relative', 
+            'z-index': 0
+            
+        }
+    } 
+    openUP(e){
+        this.setState({'min-width': '50em', 'z-index': 15, 'position': 'fixed'}) 
 
-                style={{ "display": this.props.show }}
-
-                key={this.props.show}
-            >
-                <p>Open Dog Run</p>
-                <p id="bottom">click me</p>
+    }
+    render(){ 
+        debugger
+        return(<div className="custom-kennels"
+                id={this.props.name}
+                // onClick={(e)=>this.openUP(e) }
+                style={{'maxWidth': this.props.picture, position: this.props.pos }} 
+                key={this.state['min-width']}> 
+                <img style={{'min-width': this.state['min-width'], 'z-index': this.state['z-index']}} src={this.props.pic}></img>
+                {/* <p className="dog_run">Click to Open Dog Run</p> */}
+                <p>&nbsp;</p>
             </div>
         )
     }
