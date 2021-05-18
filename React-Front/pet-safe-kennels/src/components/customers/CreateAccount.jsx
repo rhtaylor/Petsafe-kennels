c07a586712d5b98dfe8952262aff4b7afe8b7ec1
Randy <randyisbusy@gmail.com>
2021-05-18 14:26:21 -0700

    adds forms/components to create new user

--- React-Front/pet-safe-kennels/src/components/customers/CreateAccount.jsx
+++ React-Front/pet-safe-kennels/src/components/customers/CreateAccount.jsx
@@ -1,10 +1,21 @@
 import React, { Component } from 'react'
-
+import Create_Form from '../forms/Create_Form'
 export default class CreateAccount extends Component{
-    
+    constructor(props){
+        super(props)
+        this.state={
+            name: '',
+            email: '',
+            number: '',
+            city: ''
+
+        }
+    }
+
     render(){
         return (<div>
-            <h1>Create Account</h1>
+            <h1>Create Account</h1> 
+            <Create_Form formData={this.state} />
             </div>)
     }
 }
